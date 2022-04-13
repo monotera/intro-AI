ae4274a73b5b52cb527b9e5b1584551549764c6c
monotera <nelson.mosquera@javeriana.edu.co>
2022-04-13 15:45:48 -0500

    updates doc

--- Proyecto3/script.py
+++ Proyecto3/script.py
@@ -9,8 +9,27 @@ def get_sheetnames_xlsx(filepath):
 
 
 def obtain_data(filepath):
+    #Obtains sheet names from excel
     sheetnames = get_sheetnames_xlsx(filepath)
     prob_data = {}
+    """
+    - Obtains the data from each sheet in excel. 
+    - it comes as a dataFrame and then its transform into a dictionary
+    example of data:
+    prob_data = 
+    {   'Appointment': {'Train': ['on time', 'delayed'],
+                        'attend': [0.9, 0.6],
+                        'miss': [0.1, 0.4]},
+        'Maintenance': {'Rain': ['none', 'light', 'heavy'],
+                        'no': [0.6, 0.8, 0.9],
+                        'yes': [0.4, 0.2, 0.1]},
+        'Rain':        {'heavy': [0.1], 'light': [0.2], 'none': [0.7]},
+        'Train':       {'Maintenance': ['yes', 'no', 'yes', 'no', 'yes', 'no'],
+                        'Rain': ['none', 'none', 'light', 'light', 'heavy', 'heavy'],
+                        'delayed': [0.2, 0.1, 0.4, 0.3, 0.6, 0.5],
+                        'on time': [0.8, 0.9, 0.6, 0.7, 0.4, 0.5]}
+    }
+    """
     for sheetname in sheetnames:
         prob_dataFrame = (pd.read_excel(filepath, sheet_name=sheetname))
         prob_data[sheetname] = prob_dataFrame.to_dict()
